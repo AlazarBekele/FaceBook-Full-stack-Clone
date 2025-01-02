3ee854aa962ed3230e5ca2a10042e6b696618fc6
AlazarBekele <alazarthe70@gmail.com>
2025-01-02 19:11:50 +0300

    Start Sign up

--- Facebook/urls.py
+++ Facebook/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, login_page
+from .views import index, login_page, sign_up
 
 
 urlpatterns = [
     path ('', index, name='index'),
-    path ('login/', login_page, name='login')
+    path ('login/', login_page, name='login'),
+    path ('signup/', sign_up, name='signup')
 ]
